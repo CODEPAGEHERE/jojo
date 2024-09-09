ba6b1f1a7b5ff27fb2f178b337f4c870bd26a22b
jojobot <adenijimujeeb8@gmail.com>
2024-09-09 04:45:15 +0100

    not done

--- count.js
+++ count.js
@@ -5,5 +5,11 @@
         `<h2>I'm happy to let you know <br> that I have done +${num} Projects Sucessfully <br> I'm a team player.</h2>`;
 
         // Insert the dynamic message into the element with ID 'project'
+
         document.getElementById("project").innerHTML = count;
 
+    
+
+        const date = new Date();
+        
+        document.getElementById("CurrentDate").innerHTML = date.getFullYear();
